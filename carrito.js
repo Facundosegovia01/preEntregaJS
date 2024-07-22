e7eb22e2724b8cfcd366e2c630e52e1314b63380
Facundo Segovia <fsegovia06@gmail.com>
2024-07-22 13:45:48 -0300

    first commit

---
+++ carrito.js
@@ -0,0 +1,52 @@
+function vaciarCarrito() {
+    localStorage.removeItem("carrito");
+    renderCarrito();
+}
+
+function eliminarProducto(index) {
+    const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    carrito.splice(index, 1);
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    renderCarrito();
+}
+
+function renderCarrito() {
+    let contenidoHTML = `
+    <div class="flex justify-end">
+        <button class="flex items-center gap-2 rounded bg-red-600" onclick="vaciarCarrito();">Vaciar carrito
+            <svg data-testid="geist-icon" height="20" stroke-linejoin="round" viewBox="0 0 16 16" width="20" style="color: currentcolor;">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M6.75 2.75C6.75 2.05964 7.30964 1.5 8 1.5C8.69036 1.5 9.25 2.05964 9.25 2.75V3H6.75V2.75ZM5.25 3V2.75C5.25 1.23122 6.48122 0 8 0C9.51878 0 10.75 1.23122 10.75 2.75V3H12.9201H14.25H15V4.5H14.25H13.8846L13.1776 13.6917C13.0774 14.9942 11.9913 16 10.6849 16H5.31508C4.00874 16 2.92263 14.9942 2.82244 13.6917L2.11538 4.5H1.75H1V3H1.75H3.07988H5.25ZM4.31802 13.5767L3.61982 4.5H12.3802L11.682 13.5767C11.6419 14.0977 11.2075 14.5 10.6849 14.5H5.31508C4.79254 14.5 4.3581 14.0977 4.31802 13.5767Z" fill="currentColor"></path>
+            </svg>
+        </button>
+    </div>`;
+    const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+    carrito.forEach((producto, index) => {
+        contenidoHTML += `
+            <div class="w-full max-w-7xl px-4 md:px-5 lg-6 mx-auto">
+                <div class="hidden lg:grid grid-cols-2 py-6">
+                    <div class="font-normal text-xl leading-8 text-gray-200">Productos</div>
+                    <p class="font-normal text-xl leading-8 text-gray-200 flex items-center justify-between">
+                        <span class="w-full max-w-[200px] text-center">Total</span>
+                    </p>
+                </div>
+                <div class="grid grid-cols-1 lg:grid-cols-2 min-[550px]:gap-6 border-t border-gray-200 py-6">
+                    <div class="flex items-center flex-col min-[550px]:flex-row gap-3 min-[550px]:gap-6 w-full max-xl:justify-center max-xl:max-w-xl max-xl:mx-auto">
+                        <div class="img-box"><img src="img/${producto.imagen}" alt="" class="xl:w-[140px]"></div>
+                        <div class="pro-data w-full max-w-sm">
+                            <h5 class="font-semibold text-2xl leading-8 text-white max-[550px]:text-center">${producto.nombre}</h5>
+                            <p class="font-normal text-sm leading-8 text-gray-400 my-2 min-[550px]:my-3 max-[550px]:text-center">${producto.descripcion}</p>
+                            <h6 class="font-medium text-lg leading-8 text-purple-500 max-[550px]:text-center">$${producto.precio}</h6>
+                        </div>
+                    </div>
+                    <div class="flex items-center flex-col min-[550px]:flex-row w-full max-xl:max-w-xl max-xl:mx-auto gap-2">
+                        <h6 class="text-purple-500 font-manrope font-bold text-2xl leading-9 w-full max-w-[176px] text-center">$${producto.precio}</h6>
+                        <button class="border-2 rounded-lg bg-red-600 border-red-600 font-bold text-1xl w-full max-w-[176px] text-center" onclick="eliminarProducto(${index});">Eliminar [-]</button>
+                    </div>
+                </div>
+            </div>`;
+    });
+    document.getElementById("contenido").innerHTML = contenidoHTML;
+}
+
+renderCarrito();
---
+++ main.js
@@ -0,0 +1,58 @@
+const productos = [ 
+  {id: 1, nombre: "Iphone 11", descripcion: "Iphone 11 de 128 GB", precio: 310, imagen: "iphone11.jpg" },
+  {id: 2, nombre: "Iphone 11 PRO", descripcion: "Iphone 11 pro de 128 GB", precio: 310, imagen: "11promax.jpg" },
+  {id: 3, nombre: "Iphone 14", descripcion: "Iphone 14 pro de 256 GB", precio: 310, imagen: "iphone14.jpg" },
+];
+
+function renderProductos(productos) { 
+  let contenidoHTML = "";
+
+  for (const producto of productos) {
+      contenidoHTML +=  `
+      <div class="w-60 h-80 bg-gray-50 p-3 flex flex-col gap-1 rounded-2xl">
+          <img class="h-48 rounded-xl object-cover" src="img/${producto.imagen}" />
+          <div class="flex flex-col gap-4">
+              <div class="flex flex-row justify-between">
+                  <div class="flex flex-col">
+                      <span class="text-xl font-bold">${producto.nombre}</span>
+                      <span class="text-sm font-semibold text-gray-700">${producto.descripcion}</span>
+                  </div>
+                  <span class="font-bold text-purple-950">$${producto.precio}</span>
+              </div>
+              <button class="hover:bg-purple-900 text-gray-50 bg-purple-600 py-2 rounded-md" onclick="agregarProducto(${producto.id});">Add to cart</button>
+          </div>
+      </div>`;
+  }    
+
+  document.getElementById("card").innerHTML = contenidoHTML;
+}
+
+function agregarProducto(id) {
+  const producto = productos.find(item => item.id === id);
+  let carrito = JSON.parse(localStorage.getItem("carrito"));
+  
+  // Verifica que el carrito sea un array, si no lo es, inicialízalo como un array vacío
+  if (!Array.isArray(carrito)) {
+      carrito = [];
+  }
+
+  carrito.push(producto);
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  console.log("El producto se agregó correctamente");
+  totalProductos();
+}
+
+function totalProductos() { 
+  let carrito = JSON.parse(localStorage.getItem("carrito"));
+  
+  if (!Array.isArray(carrito)) {
+      carrito = [];
+  }
+  
+  document.getElementById("totalCarrito").innerHTML = carrito.length;
+  return carrito.length;
+}
+
+renderProductos(productos);
+totalProductos();
+// La llamada a agregarProducto(); sin argumentos se elimina.
